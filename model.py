10e74cee167dd39ad1fe7dbc248d29a9b777ccc7
Henry Zheng <73442345+nullZheng@users.noreply.github.com>
2023-05-28 01:21:49 -0400

    Add files via upload

---
+++ model.py
@@ -0,0 +1,117 @@
+import cv2
+import numpy as np
+import os
+import sys
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten, Conv2D
+
+EPOCHS = 10
+IMG_WIDTH = 28
+NUM_CATEGORIES = 10
+IMG_HEIGHT = 28
+TEST_SIZE = 0.3
+input_shape = IMG_WIDTH, IMG_HEIGHT, 3
+
+def main():
+    # Check command-line arguments
+    if len(sys.argv) not in [2, 3]:
+        sys.exit("Usage: python traffic.py data_directory [model.h5]")
+    # Get image arrays and labels for all image files
+    images, labels = load_data(sys.argv[1])
+
+    # Split data into training and testing sets
+    labels = tf.keras.utils.to_categorical(labels)
+    x_train, x_test, y_train, y_test = train_test_split(
+        np.array(images), np.array(labels), test_size=TEST_SIZE
+    )
+
+    # Get a compiled neural network
+    model = get_model()
+
+    # Fit model on training data
+    model.fit(x_train, y_train, epochs=EPOCHS)
+
+    # Evaluate neural network performance
+    model.evaluate(x_test,  y_test, verbose=2)
+
+    # Save model to file
+    if len(sys.argv) == 3:
+        filename = sys.argv[2]
+        model.save(filename)
+        print(f"Model saved to {filename}.")
+
+def load_data(path):
+    """
+    Load image data from directory `data_dir`.
+
+    Assume `data_dir` has one directory named after each category, numbered
+    0 through NUM_CATEGORIES - 1. Inside each category directory will be some
+    number of image files.
+
+    Return tuple `(images, labels)`. `images` should be a list of all
+    of the images in the data directory, where each image is formatted as a
+    numpy ndarray with dimensions IMG_WIDTH x IMG_HEIGHT x 3. `labels` should
+    be a list of integer labels, representing the categories for each of the
+    corresponding `images`.
+    """
+    n = 0
+    images = []
+    labels = []
+    for file in os.listdir(path):
+        file_path = os.path.join(path, file)
+        try:
+            image = np.array(cv2.resize(cv2.imread(file_path), (IMG_WIDTH, IMG_HEIGHT), interpolation=cv2.INTER_AREA))
+            images.append(image)
+            if n % 10000 == 0:
+                print(n)
+            n += 1
+            for num in file:
+                try:
+                    labels.append(int(num))
+                    break
+                except Exception:
+                    pass
+        except Exception:
+            return images, labels
+    return images, labels
+
+
+def get_model():
+    """
+    Returns a compiled convolutional neural network model. Assume that the
+    `input_shape` of the first layer is `(IMG_WIDTH, IMG_HEIGHT, 3)`.
+    The output layer should have `NUM_CATEGORIES` units, one for each category.
+    """
+    model = tf.keras.models.Sequential([
+
+        tf.keras.layers.Conv2D(filters=32, kernel_size=(3,3),activation='relu',input_shape=input_shape),
+        tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(pool_size=(1, 1)),
+        tf.keras.layers.Dropout(rate=0.3),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Conv2D(filters=128, kernel_size=(3, 3),activation='relu'),
+        tf.keras.layers.Conv2D(filters=64, kernel_size=(3, 3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(pool_size=(1, 1)),
+        tf.keras.layers.Dropout(rate=0.3),
+        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(12*NUM_CATEGORIES, activation='relu'),
+        tf.keras.layers.Dense(NUM_CATEGORIES, activation='relu'),
+        tf.keras.layers.Dense(units=NUM_CATEGORIES, activation='softmax')
+    ])
+
+    # Train neural network
+    model.compile(
+        optimizer="adam",
+        loss="categorical_crossentropy",
+        metrics=["accuracy"]
+    )
+    return model
+if __name__ == "__main__":
+    main()
